bef34bb08a9938dc3c63a387420e4e28865450c1
Fronz <z233@msn.com>
2020-09-15 23:22:59 +0800

    添加首页第三屏作品瀑布流

---
+++ js/index.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function ($) {
+    const slidepage = new slidePage();
+    const $container = $('.works-container').masonry({
+        columnWidth: 290,
+        itemSelector: '.works',
+        isFitWidth: true
+    })
+});
